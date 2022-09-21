de95d7996bc94e1c85ad57f6f3b44a594c7f9fc6
Gabriel Do Carmo <83257171+G7Carmo@users.noreply.github.com>
2022-09-21 19:45:40 -0300

    refactoring xml card favorite

--- app/src/main/java/com/gdsdesenvolvimento/wefit/ui/view/adapter/FavoriteAdapter.kt
+++ app/src/main/java/com/gdsdesenvolvimento/wefit/ui/view/adapter/FavoriteAdapter.kt
@@ -8,18 +8,18 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.gdsdesenvolvimento.wefit.data.model.db.InfoRepo
-import com.gdsdesenvolvimento.wefit.databinding.RvItemFavoriteBinding
+import com.gdsdesenvolvimento.wefit.databinding.CardFavoriteBinding
 
 class FavoriteAdapter(
     private val listItems: List<InfoRepo>,
     private val fragment : Fragment
 ) : RecyclerView.Adapter<FavoriteAdapter.FavoriteViewHolder>() {
-    inner class FavoriteViewHolder(val binding: RvItemFavoriteBinding) :
+    inner class FavoriteViewHolder(val binding: CardFavoriteBinding) :
         RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteViewHolder {
         return FavoriteViewHolder(
-            RvItemFavoriteBinding.inflate(
+            CardFavoriteBinding.inflate(
                 LayoutInflater.from(parent.context), parent, false
             )
         )
